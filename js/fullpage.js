57e690a4c25b27e85d7231086683a7441cfeb65b
Szymon <szymon.grotha@gmail.com>
2016-12-05 14:55:45 +0100

    Portfolio Website First Commit

---
+++ js/fullpage.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        anchors:['home-section', 'about-section', 'skills-section', 'folio-section', 'contact-section']
+    });
+});
+
---
+++ js/menu.js
@@ -0,0 +1,29 @@
+document.getElementById('hamburger').addEventListener('click', checkNav);
+document.getElementById('home').addEventListener('click', checkOpen);
+document.getElementById('about').addEventListener('click', checkOpen);
+document.getElementById('folio').addEventListener('click', checkOpen);
+document.getElementById('skills').addEventListener('click', checkOpen);
+document.getElementById('contact').addEventListener('click', checkOpen);
+
+
+function checkNav() {
+  if (document.body.classList.contains('hamburger-active')) {
+    closeNav();
+  } else {
+    openNav();
+  }
+}
+
+function closeNav() {
+  document.body.classList.remove('hamburger-active');
+}
+
+function openNav() {
+  document.body.classList.add('hamburger-active');
+}
+
+function checkOpen() {
+    document.body.classList.remove('hamburger-active');
+}
+
+
